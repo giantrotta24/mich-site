bc7ecf26d3de857f213fb346d6373713acdbc2d4
Gian Trotta <giantrotta24@gmail.com>
2022-08-06 23:02:48 -0400

    better coming soon

---
+++ src/components/common/ComingSoon.tsx
@@ -0,0 +1,9 @@
+const ComingSoon: React.FC = () => {
+  return (
+    <h1 className="text-2xl">
+      Coming Soon <span>🚧</span>
+    </h1>
+  );
+};
+
+export default ComingSoon;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next';
 
+import ComingSoon from '../components/common/ComingSoon';
+
 const Home: NextPage = () => {
   return (
     <div className="antialiased min-h-[80vh] flex flex-col items-center justify-center">
-      <p>Coming Soon</p>
+      <ComingSoon />
     </div>
   );
 };

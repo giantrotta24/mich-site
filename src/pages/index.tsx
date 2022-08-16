29da8f99266e37ae0006b6c83617f6b4d38f1e1e
Michael Mastrucci <michael.mastrucci@gmail.com>
2022-08-16 09:11:46 -0400

    changed component name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
-import TripleImage from '../components/common/TripleImage'
+import ImageMasthead from '../components/common/ImageMasthead'
 import ComingSoon from '../components/common/ComingSoon';
 
 const Home: NextPage = () => {
   return (
     <div className="antialiased min-h-[80vh] flex flex-col items-center justify-center">
-      <TripleImage
+      <ImageMasthead
         alt />
     </div>
   );

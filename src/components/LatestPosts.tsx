ee28ad2969bd99bbb4512fa62bf27f3d2ca316f1
Gian Trotta <giantrotta24@gmail.com>
2022-08-20 19:02:02 -0400

    make sample article

--- src/components/LatestPosts.tsx
+++ src/components/LatestPosts.tsx
@@ -1,12 +1,38 @@
-import ComingSoon from './common/ComingSoon';
+/* eslint-disable @next/next/no-img-element */
+// we'll want to use the native image component when we have actual images coming from the server or from the filesystem
+import Image from 'next/image';
+
+const latestPosts = [
+  {
+    id: 1,
+    title: 'Lorem ipsum dolor sit amet',
+    date: 'Aug 24, 2022',
+    image: 'https://via.placeholder.com/250x150',
+  },
+  {
+    id: 2,
+    title: 'Lorem ipsum dolor sit amet',
+    date: 'Aug 24, 2022',
+    image: 'https://via.placeholder.com/250x150',
+  },
+  {
+    id: 3,
+    title: 'Lorem ipsum dolor sit amet',
+    date: 'Aug 24, 2022',
+    image: 'https://via.placeholder.com/250x150',
+  },
+];
 
 const LatestPosts: React.FC = () => {
   return (
     <div className="border border-red-500 grid text-center">
-      <h2 className="text-xl ">My Latest Posts</h2>
-      <div>post 1</div>
-      <div>post 2</div>
-      <div>post 3</div>
+      <h2 className="text-xl font-bold pt-2 md:pt-4">My Latest Posts</h2>
+      <div className="bg-[#f4cea5] my-8 mx-8 text-left flex flex-col">
+        <img src="https://via.placeholder.com/250x150" alt="placeholder" />
+        <p className="p-3 text-sm text-stone-600">Aug 24, 2022</p>
+        <h3 className="px-3 mb-3 font-bold text-stone-700">Title</h3>
+      </div>
+
       <div className="border border-blue-500">Button</div>
     </div>
   );

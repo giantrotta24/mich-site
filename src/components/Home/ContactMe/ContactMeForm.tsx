343bc782b31a0143935e756d1c77d510940b842d
Gian Trotta <giantrotta24@gmail.com>
2022-08-26 20:26:34 -0400

    add react hooks form, console log form results

--- src/components/Home/ContactMe/ContactMeForm.tsx
+++ src/components/Home/ContactMe/ContactMeForm.tsx
@@ -1,9 +1,16 @@
+import { useForm } from 'react-hook-form';
 import { HiOutlinePaperAirplane } from 'react-icons/hi';
 
 const ContactMeForm: React.FC = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="text-left my-4 px-6 grid gap-2">
           <div>
             <label
@@ -13,8 +20,8 @@ const ContactMeForm: React.FC = () => {
               Full Name
             </label>
             <input
+              {...register('name')}
               type="text"
-              name="name"
               id="name"
               className="p-1 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
               placeholder="Your full name here"
@@ -29,7 +36,7 @@ const ContactMeForm: React.FC = () => {
             </label>
             <input
               type="email"
-              name="email"
+              {...register('email')}
               id="email"
               className="p-1 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
               placeholder="youremail@example.com"
@@ -45,7 +52,7 @@ const ContactMeForm: React.FC = () => {
             <div className="mt-1">
               <textarea
                 id="message"
-                name="message"
+                {...register('message')}
                 rows={3}
                 className="p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
                 placeholder="What is your go to for toxin-free detergent? I'm looking for a new product that is safe for pets and people."
@@ -53,10 +60,7 @@ const ContactMeForm: React.FC = () => {
             </div>
           </div>
         </div>
-        <button
-          type="button"
-          className="border border-stone-400 mx-auto px-8 py-1 flex hover:bg-stone-200 hover:border-black"
-        >
+        <button className="border border-stone-400 mx-auto px-8 py-1 flex hover:bg-stone-200 hover:border-black">
           Send!{' '}
           <HiOutlinePaperAirplane className="h-5 w-5 ml-2" aria-hidden="true" />
         </button>

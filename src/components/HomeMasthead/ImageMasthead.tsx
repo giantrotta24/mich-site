be3bd365d4254dd926b0bf95c0aa7473f085768d
Michael Mastrucci <michael.mastrucci@gmail.com>
2022-08-28 11:51:22 -0400

    finalized responsive styles for logo and image alignment and display

--- src/components/HomeMasthead/ImageMasthead.tsx
+++ src/components/HomeMasthead/ImageMasthead.tsx
@@ -6,18 +6,18 @@ import booksPlaceholderLg from '/public/images/books-placeholder-lg.jpg';
 
 const ImageMasthead: React.FC = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-6 w-full lg:mb-44 sm:mb-26 sm:mt-0 md:mt-8'>
-      <div className='flex flex-col xl:flex-row w-full bg-white sm:hidden md:block rounded-lg'>
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-6 w-full lg:mb-44 sm:mb-26 sm:mt-0 md:mt-8 md:mb-36 mb-40'>
+      <div className='flex md:flex-col xl:flex-row md:w-full bg-white hidden md:block rounded-lg'>
         <Image
           alt='Michelle&apos;s Blog'
-          className='object-cover w-full h-48 sm-hidden'
+          className='object-cover w-full h-48'
           src={blogPlaceholder}
           placeholder='blur'
         />
       </div>
 
-      <div className='relative w-full rounded-lg'>
-        <div className='flex flex-col xl:flex-row w-full rounded-lg bg-white-lg z-0'>
+      <div className='relative w-full rounded-lg md:block'>
+        <div className='md:flex flex-col xl:flex-row w-full rounded-lg bg-white-lg z-0'>
           <Image
             alt='About Michelle'
             className='object-cover w-full z-10'
@@ -25,7 +25,7 @@ const ImageMasthead: React.FC = () => {
             placeholder='blur'
           />
 
-        <div className='flex justify-center items-center z-10 absolute sm:relative md:absolute 2xl:top-96 2xl:mt-36 lg:top-72 lg:mt-14 lg:pt-0 md:pt-2 md:left-0 md:top-36 sm:bottom-24 drop-shadow-lg'>
+        <div className='flex justify-center items-center z-10 absolute sm:relative md:absolute xl:top-96 2xl:mt-36 lg:top-72 lg:mt-14 lg:pt-0  md:top-full md:bottom-0 sm:bottom-24 sm:m-auto sm:left-0 sm:right-0 drop-shadow-lg -bottom-20'>
           <Image
               alt='Fresh beginnings logo'
               className='object-contain w-full'
@@ -38,10 +38,10 @@ const ImageMasthead: React.FC = () => {
         
       </div>
 
-      <div className='flex flex-col xl:flex-row w-full bg-white sm:hidden md:block rounded-lg'>
+      <div className='flex md:flex-col xl:flex-row md:w-full bg-white hidden md:block rounded-lg'>
         <Image
           alt='Michelle&apos;s Books'
-          className='object-cover w-full h-48 sm-hidden'
+          className='object-cover w-full h-48'
           src={booksPlaceholderLg}
           placeholder='blur'
         />

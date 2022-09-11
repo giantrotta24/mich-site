036edb460af29ecb9f97c86b7eaab0f630b084b7
Michael Mastrucci <michael.mastrucci@gmail.com>
2022-09-11 12:25:53 -0400

    added contact form to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next';
 
 import HomeMasthead from '../components/HomeMasthead';
 import LatestPosts from '../components/Home/LatestPosts/index';
+import Contact from '../components/Home/ContactMe'
 
 const Home: NextPage = () => {
   return (
     <>
       <HomeMasthead />
       <LatestPosts />
+      <Contact />
     </>
   );
 };

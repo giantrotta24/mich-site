4012dbf9904fc7223dd7861d419910287b5e7050
Gian Trotta <giantrotta24@gmail.com>
2022-09-18 13:07:05 -0400

    remove files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 import HomeMasthead from '../components/Home/HomeMasthead';
 import AboutMe from '../components/Home/AboutMe';
 import LatestPosts from '../components/Home/LatestPosts/index';
-import Contact from '../components/Home/ContactMe';
+import ContactMe from '../components/Home/ContactMe';
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
       <HomeMasthead />
       <LatestPosts />
       <AboutMe />
-      <Contact />
+      <ContactMe />
     </>
   );
 };

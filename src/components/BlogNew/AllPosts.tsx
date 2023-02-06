07bfdf4ba66e7f5aff5ea88db049eee3346da397
Gian Trotta <giantrotta24@gmail.com>
2023-02-05 21:52:07 -0500

    post preview for all posts

--- src/components/BlogNew/AllPosts.tsx
+++ src/components/BlogNew/AllPosts.tsx
@@ -1,4 +1,5 @@
 import type { Post } from 'lib/sanity.queries';
+
 import PostPreview from './PostPreview';
 
 const AllPosts = ({ posts }: { posts: Post[] }) => {
@@ -7,9 +8,17 @@ const AllPosts = ({ posts }: { posts: Post[] }) => {
       <h3 className="font-['Dancing_Script'] mt-2 mb-4 text-3xl lg:text-4xl font-bold text-center">
         More Posts
       </h3>
-      <div className="mb-32 grid grid-cols-1 gap-y-20 md:grid-cols-2 md:gap-x-16 md:gap-y-32 lg:gap-x-32">
+      <div className="mb-32 grid grid-cols-1 gap-y-12 md:grid-cols-2 md:gap-x-12 md:gap-y-22 lg:gap-x-28">
         {posts.map(post => (
-          <PostPreview key={post._id} title={post.title} />
+          <PostPreview
+            key={post._id}
+            title={post.title}
+            coverImage={post.coverImage}
+            date={post.date}
+            author={post.author}
+            slug={post.slug}
+            excerpt={post.excerpt}
+          />
         ))}
       </div>
     </div>
--- src/components/BlogNew/PostPreview.tsx
+++ src/components/BlogNew/PostPreview.tsx
@@ -1,5 +1,48 @@
-const PostPreview = ({ title }) => {
-  return <div>{title}</div>;
+import Link from 'next/link';
+
+import type { Post } from 'lib/sanity.queries';
+
+import CoverImage from './CoverImage';
+import Date from 'components/Blog/PostDate';
+
+const PostPreview = ({
+  title,
+  coverImage,
+  date,
+  excerpt,
+  author,
+  slug,
+}: Omit<Post, '_id'>) => {
+  return (
+    <div className="p-4 bg-stone-100">
+      <div className="mb-8">
+        <CoverImage
+          slug={slug}
+          title={title}
+          image={coverImage}
+          priority={false}
+        />
+      </div>
+      <div>
+        <h3 className="mb-2 text-2xl lg:text-4xl font-bold">
+          <Link href={`/blog/${slug}`} className="hover:underline">
+            {title || 'Untitled'}
+          </Link>
+        </h3>
+        <div className="text-md md:text-sm text-stone-600 mb-1">
+          <Date dateString={date} />
+        </div>
+        <div>
+          {excerpt && <p className="mb-2 text-lg leading-relaxed">{excerpt}</p>}
+          {author && (
+            <div className="text-stone-600 text-sm">
+              By <span className="font-bold">{author.name}</span>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostPreview;
